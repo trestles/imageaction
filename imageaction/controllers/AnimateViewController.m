1fa8ade3dafca7a4d35190fbab28e82ce48586f6
jonathan twaddell <jon@arclocal.com>
2014-09-23 11:52:15 -0700

    this is updated

---
+++ imageaction/controllers/AnimateViewController.m
@@ -0,0 +1,41 @@
+//
+//  AnimateViewController.m
+//  imageaction
+//
+//  Created by jonathan twaddell on 9/23/14.
+//  Copyright (c) 2014 Embers. All rights reserved.
+//
+
+#import "AnimateViewController.h"
+
+@interface AnimateViewController ()
+
+@end
+
+@implementation AnimateViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)dismiss:(id)sender {
+  [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
